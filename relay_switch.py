f8e32f545245b19cb26b089a1bfdfab7acb433e6
cmnworks <chris.natan@gmail.com>
2018-10-14 02:07:55 +0800

    Pushed - new version

--- relay_switch.py
+++ relay_switch.py
@@ -1,37 +1,37 @@
-# Copyright (c) CMNWorks
-# Licensed under The MIT License
-#
-# @copyright     Copyright (c) CMNWorks Christopher M. Natan
 # @author        Christopher M. Natan
-# @link          http://cmnworks.com
-# @license       http://www.opensource.org/licenses/mit-license.php MIT License
+# @version       1.0
 
 import RPi.GPIO as GPIO
-
-
-GPIO_PIN = 14
+GPIO_RELAY_PIN = 14
 
 
 class RelaySwitch:
     def __init__(self):
         pass
 
-    def turn(self, state):
-        GPIO.setup(GPIO_PIN, GPIO.IN)
-        GPIO.setup(GPIO_PIN, GPIO.OUT)
+    def set_gpio(self):
+        print("Include amount gpio initialize..")
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(GPIO_RELAY_PIN, GPIO.OUT)
 
-        if state == 'off':
-            print("I am off")
-            GPIO.output(pin_switch, GPIO.LOW)
-            self.get_state()
+    def turn(self, action):
+        if action == 'on': self.on()
+        if action == 'off': self.off()
+
+    def on(self):
+        self.set_gpio()
+        if  self.get_state() == 1:
+            print ('Turning on')
+            GPIO.output(GPIO_RELAY_PIN, GPIO.HIGH)
+
+    def off(self):
+        self.set_gpio()
+        if  self.get_state() == 0:
+            print ('Turning off')
             GPIO.cleanup()
 
-        if state == 'on':
-            print("I am on")
-            GPIO.output(pin_switch, GPIO.HIGH)
-            self.get_state()
-
     def get_state(self):
-        switch_state = GPIO.input(pin_switch)
-        print('Current state is ' + str(switch_state))
-        return switch_state
+        state = GPIO.input(GPIO_RELAY_PIN)
+        print('Current state is ' + str(state))
+        return state
