253f2e2fa6a59c0b12b182b43b4daf179dea611c
cmnworks <chris.natan@gmail.com>
2018-10-14 06:18:25 +0800

    Update relay_switch.py

--- relay_switch.py
+++ relay_switch.py
@@ -7,10 +7,9 @@ GPIO_RELAY_PIN = 14
 
 class RelaySwitch:
     def __init__(self):
-        pass
+        self.set_gpio()
 
     def set_gpio(self):
-        print("Include amount gpio initialize..")
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(GPIO_RELAY_PIN, GPIO.OUT)
@@ -20,13 +19,11 @@ class RelaySwitch:
         if action == 'off': self.off()
 
     def on(self):
-        self.set_gpio()
         if  self.get_state() == 1:
             print ('Turning on')
             GPIO.output(GPIO_RELAY_PIN, GPIO.HIGH)
 
     def off(self):
-        self.set_gpio()
         if  self.get_state() == 0:
             print ('Turning off')
             GPIO.cleanup()
